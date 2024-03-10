27054eb479cddbd0fb5ee9ff33db7459c821ee9f
Emerson <lopes.ds@live.com>
2024-03-10 12:30:02 -0300

    chore() adicionando consulta restaurantes

---
+++ src/main/java/com/fiap/reservarest/adapter/restaurant/RestaurantsEnum.java
@@ -0,0 +1,45 @@
+package com.fiap.reservarest.adapter.restaurant;
+
+public enum RestaurantsEnum {
+
+    RESTAURANTE_A("Restaurante da Nonna", CuisineTypeEnum.ITALIANA, "Rua das Flores, 123", "08:00 - 22:00", 50),
+    RESTAURANTE_B("Sushi Express", CuisineTypeEnum.JAPONESA, "Avenida Central, 456", "11:00 - 23:00", 40),
+    RESTAURANTE_C("Taquería del Sol", CuisineTypeEnum.MEXICANA, "Rua dos Tacos, 789", "10:00 - 21:00", 60),
+    RESTAURANTE_D("Burger Haven", CuisineTypeEnum.FAST_FOOD, "Rua das Hamburguerias, 101", "09:00 - 00:00", 30),
+    RESTAURANTE_E("Taj Mahal", CuisineTypeEnum.INDIANA, "Avenida das Especiarias, 210", "12:00 - 23:00", 70),
+    RESTAURANTE_F("Comida da vóvó", CuisineTypeEnum.BRASILEIRA, "Rua dos Sabores, 15", "07:00 - 20:00", 20);
+
+    private final String name;
+    private final CuisineTypeEnum cuisineType;
+    private final String location;
+    private final String hoursOfOperation;
+    private final int capacity;
+
+    RestaurantsEnum(String name, CuisineTypeEnum cuisineType, String location, String hoursOfOperation, int capacity) {
+        this.name = name;
+        this.cuisineType = cuisineType;
+        this.location = location;
+        this.hoursOfOperation = hoursOfOperation;
+        this.capacity = capacity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public CuisineTypeEnum getCuisineType() {
+        return cuisineType;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getHoursOfOperation() {
+        return hoursOfOperation;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+}
--- src/main/java/com/fiap/reservarest/application/entrypoint/rest/restaurant/RestaurantController.java
+++ src/main/java/com/fiap/reservarest/application/entrypoint/rest/restaurant/RestaurantController.java
@@ -3,22 +3,22 @@ package com.fiap.reservarest.application.entrypoint.rest.restaurant;
 import com.fiap.reservarest.adapter.restaurant.mapper.RestaurantMapper;
 import com.fiap.reservarest.application.entrypoint.rest.restaurant.dto.RestaurantRequestDTO;
 import com.fiap.reservarest.domain.restaurant.usecase.RestaurantCreationUseCase;
+import com.fiap.reservarest.domain.restaurant.usecase.RestaurantSearchUseCase;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/restaurants")
 public class RestaurantController {
 
     private final RestaurantCreationUseCase restaurantCreationUseCase;
+    private final RestaurantSearchUseCase restaurantSearchUseCase;
 
-    public RestaurantController(RestaurantCreationUseCase restaurantCreationUseCase) {
+    public RestaurantController(RestaurantCreationUseCase restaurantCreationUseCase, RestaurantSearchUseCase restaurantSearchUseCase) {
         this.restaurantCreationUseCase = restaurantCreationUseCase;
+        this.restaurantSearchUseCase = restaurantSearchUseCase;
     }
 
     @PostMapping
@@ -31,5 +31,23 @@ public class RestaurantController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(RestaurantMapper.toResponseDTO(output));
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<?> searchRestaurant(
+            @RequestParam(required = false) final String keyword
+    ) {
+        final var domainEntity = RestaurantMapper.toDomainEntity(keyword);
+        final var output = restaurantSearchUseCase.execute(domainEntity);
+
+        final var response = RestaurantMapper.toResponseDTO(output);
+
+        if (output.isEmpty())
+            return ResponseEntity
+                    .status(HttpStatus.NO_CONTENT).body(response);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(response);
     }
 }
