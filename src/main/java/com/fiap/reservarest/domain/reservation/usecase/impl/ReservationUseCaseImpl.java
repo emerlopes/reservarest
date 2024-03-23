bbd752e0b23887633777b732deae5a6594b7b22e
Emerson <lopes.ds@live.com>
2024-03-23 13:48:23 -0300

    chore() implementando verificacao de status reserva

--- src/main/java/com/fiap/reservarest/domain/reservation/usecase/impl/ReservationUseCaseImpl.java
+++ src/main/java/com/fiap/reservarest/domain/reservation/usecase/impl/ReservationUseCaseImpl.java
@@ -1,7 +1,9 @@
 package com.fiap.reservarest.domain.reservation.usecase.impl;
 
+import com.fiap.reservarest.adapter.reservation.entity.ReservationStatusEnum;
 import com.fiap.reservarest.adapter.reservation.mapper.ReservationMapper;
 import com.fiap.reservarest.domain.reservation.entity.ReservationDomainEntity;
+import com.fiap.reservarest.domain.reservation.exception.ReservationDomainCustomException;
 import com.fiap.reservarest.domain.reservation.service.ReservationService;
 import com.fiap.reservarest.domain.reservation.usecase.ReservationUseCase;
 import com.fiap.reservarest.domain.restaurant.service.RestaurantService;
@@ -23,6 +25,13 @@ public class ReservationUseCaseImpl implements ReservationUseCase {
     @Override
     @Transactional
     public ReservationDomainEntity execute(ReservationDomainEntity reservationDomainEntity) {
+
+        final var reservationStatus = reservationDomainEntity.getStatus();
+
+        if (reservationStatus != null && reservationStatus.equals(ReservationStatusEnum.CANCELED)) {
+            throw new ReservationDomainCustomException("Status cannot be canceled for reservation creation");
+        }
+
         final var restaurantExternalId = reservationDomainEntity.getRestaurantExternalId();
         final var restaurantDomainEntity = restaurantService.findByExternalId(restaurantExternalId);
         final var domainEntity = ReservationMapper.toDomainEntity(reservationDomainEntity, restaurantDomainEntity);
