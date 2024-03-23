65ec87e6add822882a87ca91bbd29d82bbaa7d37
Emerson <lopes.ds@live.com>
2024-03-23 20:26:22 -0300

    chore() implementando exception

--- src/main/java/com/fiap/reservarest/application/exception/GlobalException.java
+++ src/main/java/com/fiap/reservarest/application/exception/GlobalException.java
@@ -1,6 +1,7 @@
 package com.fiap.reservarest.application.exception;
 
 import com.fiap.reservarest.domain.reservation.exception.ReservationDomainCustomException;
+import com.fiap.reservarest.domain.restaurant.exception.RestaurantDomainCustomException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -26,6 +27,17 @@ public class GlobalException {
     @ExceptionHandler(ReservationDomainCustomException.class)
     public ResponseEntity<Map<String, Object>> handleExceptionBookingTable(
             ReservationDomainCustomException exception
+    ) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("message", exception.getMessage());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(body);
+    }
+
+    @ExceptionHandler(RestaurantDomainCustomException.class)
+    public ResponseEntity<Map<String, Object>> handleExceptionBookingTable(
+            RestaurantDomainCustomException exception
     ) {
         Map<String, Object> body = new LinkedHashMap<>();
         body.put("timestamp", LocalDateTime.now());
